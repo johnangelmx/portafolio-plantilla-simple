5bc550c26669fab4b0062eac3f58bd5d0fa6b251
unknown <johnangelmx@gmail.com>
2021-07-13 15:01:12 -0500

    make it better

---
+++ index.php
@@ -0,0 +1,263 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Eaabr | Portafolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="toggle" onclick="toggleMenu();"></div>
+    <section class="banner" id="home">
+        <div class="imgSidebar">
+            <img src="images/bg.jpg" alt="Presentacion Imagen">
+        </div>
+        <div class="contentBx">
+            <h5 class="logoText">Mi_Portafolio</h5>
+            <div>
+                <h4><span>Hola</span>, Soy...</h4>
+                <h2>JohnAngelMx</h2>
+                <h4>Im A Front End Developer </h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Maiores nihil temporibus corporis, accusamus sapiente,
+                    tenetur repellat eius sequi fuga error itaque iure
+                    veritatis. Labore cumque ipsam debitis aperiam
+                    doloremque animi.</p>
+                <a href="#" class="btn">About Me</a>
+            </div>
+            <ul class="sci">
+                <li><a href="#"></a><img src="images/twitter.png" alt="facebook Imagen"></a></li>
+                <li><a href="#"></a><img src="images/facebook.png" alt="facebook Imagen"></a></li>
+                <li><a href="#"></a><img src="images/instagram.png" alt="facebook Imagen"></a></li>
+
+            </ul>
+        </div>
+    </section>
+    <!-- <div class="toggle" onclick="toggleMenu();"></div>
+    <section class="banner" id="home">
+        <div class="imgSiderbar">
+            <img src="images/bg.jpg" alt="">
+        </div>
+        <div class="contentBx">
+            <h5 class="logoText">My_Portafolio</h5>
+            <div>
+                <h4>Hello<span>, I'm</span></h4>
+                <h2>Muhammaad Irishad</h2>
+                <h4>I'm a Front End Developer.</h4>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, odit quos sunt error qui magni
+                    vitae nesciunt a culpa sit, nisi repellat omnis voluptate! Velit atque laboriosam maiores hic ullam.
+                    Omnis iusto earum necessitatibus </p>
+            </div>
+        </div>
+    </section> -->
+    <section class="about" id="about">
+        <div class="title white">About
+            <h2>About Us</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+        </div>
+        <div class="content">
+            <div class="textBx">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate tempore delectus sint? Odit rem
+                    ipsam adipisci tempore blanditiis distinctio, ad consequatur ratione provident in fugit facilis.
+                    Esse incidunt dolorem distinctio! Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Consectetur, saepe.
+                    <br><br>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus, neque. Molestiae asperiores dolor
+                    voluptates ut, rerum quidem a aspernatur cupiditate accusantium! Ullam quisquam officiis quis
+                    repellendus sed modi expedita perspiciatis? Lorem ipsum, dolor sit amet consectetur adipisicing
+                    elit. Cupiditate, iusto!
+                </p>
+            </div>
+            <div class="imgBx">
+                <img src="images/bg1.jpg" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section class="services" id="services">
+        <div class="title">
+            <h2>Our Services</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae velit, ipsum magnam aliquam
+                distinctio, facilis suscipit quae deleniti cumque explicabo .</p>
+
+        </div>
+        <div class="content">
+            <div class="servicesBx">
+                <img src="images/icon1.png" alt="">
+                <h2>Web Desing</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div>
+            <div class="servicesBx">
+                <img src="images/icon2.png" alt="">
+                <h2>Web Development</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div>
+            <div class="servicesBx">
+                <img src="images/icon3.png" alt="">
+                <h2>Android Apps</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div>
+            <div class="servicesBx">
+                <img src="images/icon4.png" alt="">
+                <h2>Photography</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div>
+            <div class="servicesBx">
+                <img src="images/icon5.png" alt="">
+                <h2>Content Writing</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div>
+            <div class="servicesBx">
+                <img src="images/icon6.png" alt="">
+                <h2>Video Editing</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur animi repudiandae, a aliquam
+                    explicabo optio .</p>
+            </div </div>
+    </section>
+
+    <section class="work" id="work">
+        <div class="title">
+            <h2>Recent Work</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae velit, ipsum magnam aliquam
+                distinctio, facilis suscipit quae deleniti cumque explicabo .</p>
+        </div>
+        <div class="content">
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img1.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project01</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img2.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project02</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img3.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project03</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img4.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project04</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img5.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project05</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img6.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project06</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img7.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project07</h3>
+                </div>
+            </div>
+            <div class="workBx">
+                <div class="imgBx">
+                    <img src="images/img8.jpg" alt="">
+                </div>
+                <div class="textBx">
+                    <h3>Project08</h3>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="contact" id="contact">
+        <div class="title white">
+            <h2>Contact Us</h2>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur molestiae velit, ipsum magnam aliquam
+                distinctio, facilis suscipit quae deleniti cumque explicabo .</p>
+        </div>
+        <div class="contactForm">
+            <div class="row">
+                <div class="col50">
+                    <input type="text" name="" placeholder="First Name">
+                </div>
+                <div class="col50">
+                    <input type="text" name="" placeholder="Last Name">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col50">
+                    <input type="text" name="" placeholder="Email Address">
+                </div>
+                <div class="col50">
+                    <input type="text" name="" placeholder="Mobile no.">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col100">
+                    <textarea placeholder="Message"></textarea>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col100">
+                    <input type="submit" name="" value="Send">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="copyright" id="copyright">
+        <p>Copyright &copy:2021 JohnAngelMx. Derechos Reservados</p>
+    </div>
+
+    <div class="sidebar">
+        <ul class="menu">
+            <li><a href="#home" onclick="toggleMenu();">Home</a></li>
+            <li><a href="#about" onclick="toggleMenu();">About</a></li>
+            <li><a href="#services" onclick="toggleMenu();">Services</a></li>
+            <li><a href="#work" onclick="toggleMenu();">Work</a></li>
+            <li><a href="#copyright" onclick="toggleMenu();">Contact</a></li>
+        </ul>
+    </div>
+
+
+    <script> // Funcion para abrir el slid menu ,convirtiendo en activa , asi pudiendo manipularlo en css
+        function toggleMenu() {
+            const menuToggle = document.querySelector('.toggle');
+            const sidebar = document.querySelector('.sidebar');
+            menuToggle.classList.toggle('active')
+            sidebar.classList.toggle('active')
+        }
+    </script>
+</body>
+
+</html>
